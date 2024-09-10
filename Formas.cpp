74bc7a8c24fbf50d3cc34f341dbe3acff6e2c9d7
Fitzgerald Garcia <82980326+FitzL@users.noreply.github.com>
2024-09-10 12:32:55 -0400

    added derived shapes

--- Formas.cpp
+++ Formas.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#define PI 3.141592653589793238462643383279 //30 digitos, eso da
 
 using namespace std;
 
-class Shape { //Lo quice hacer en ingles(vars), espero que eso no importe...
+class Shape { //Está todo en inglés...
   protected: 
     double Height, Width;
 
   public:
-    double GetArea(){
-      return Height * Width;
-    }
+    // Methods
+    virtual double CalculateSurface() = 0;
 
+    //Getters
     double GetHeight() {
       return Height;
     }
@@ -19,6 +20,7 @@ class Shape { //Lo quice hacer en ingles(vars), espero que eso no importe...
       return Width;
     }
 
+    //Setters
     void SetHeight(double H) {
       Height = H;
     }
@@ -27,22 +29,76 @@ class Shape { //Lo quice hacer en ingles(vars), espero que eso no importe...
       Width = W;
     }
     
-    Shape(double Height, double Width){
+    //Constructor
+    Shape(double Height, double Width = -1){
 	SetHeight(Height);
 	SetWidth(Width);
     }
 };
 
+class Triangle : public Shape{
+  public:
+    double CalculateSurface(){
+      return Height * Width * 0.5d;
+    }
+
+  using Shape::Shape; //Jalo el mismo constructor
+};
+
+class Rectangle : public Shape{
+  public:
+    double CalculateSurface(){
+      return Height * Width;
+    }
+
+  using Shape::Shape; 
+};
+
+class Circle : public Shape{
+  public:
+    double CalculateSurface(){
+      return (Height) * (Height) * PI;
+    }
+
+    void SetRadius(double r) {
+      Height = r;
+      Width = r;
+    }
+
+    Circle(double r) : Shape(r, r) { }
+};
+
 int main (){
   cout << "Hello World!" << endl;
-  Shape test(2,3);
+  Triangle test(5, 3);
   cout << test.GetHeight() << endl;
   cout << test.GetWidth() << endl;
-  cout << test.GetArea() << endl;
+  cout << test.CalculateSurface() << endl;
 
   test.SetWidth(10);
-  test.SetHeight(2);
+  test.SetHeight(4);
   cout << test.GetHeight() << endl;
   cout << test.GetWidth() << endl;
-  cout << test.GetArea() << endl;
+  cout << test.CalculateSurface() << endl;
+
+  Rectangle test1(5, 3);
+  cout << test1.GetHeight() << endl;
+  cout << test1.GetWidth() << endl;
+  cout << test1.CalculateSurface() << endl;
+
+  test1.SetWidth(10);
+  test1.SetHeight(4);
+  cout << test1.GetHeight() << endl;
+  cout << test1.GetWidth() << endl;
+  cout << test1.CalculateSurface() << endl;
+
+  Circle test2(53);
+  cout << test2.GetHeight() << endl;
+  cout << test2.GetWidth() << endl;
+  cout << test2.CalculateSurface() << endl;
+
+  test2.SetRadius(4);
+  cout << test2.GetHeight() << endl;
+  cout << test2.GetWidth() << endl;
+  cout << test2.CalculateSurface() << endl;
 }
