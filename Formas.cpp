f93d5ff5de5111dd80b2435c21aa07dfc225dbfc
Fitzgerald Garcia <82980326+FitzL@users.noreply.github.com>
2024-09-10 21:01:06 -0400

    Shape vector added to source

--- Formas.cpp
+++ Formas.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #define PI 3.141592653589793238462643383279 //30 digitos, eso da
 
 using namespace std;
@@ -70,35 +71,30 @@ class Circle : public Shape{
 
 int main (){
   cout << "Hello World!" << endl;
-  Triangle test(5, 3);
+
+  Triangle test(5, 3), *t0 = &test;
   cout << test.GetHeight() << endl;
   cout << test.GetWidth() << endl;
   cout << test.CalculateSurface() << endl;
 
-  test.SetWidth(10);
-  test.SetHeight(4);
-  cout << test.GetHeight() << endl;
-  cout << test.GetWidth() << endl;
-  cout << test.CalculateSurface() << endl;
-
-  Rectangle test1(5, 3);
+  Rectangle test1(5, 3), *t1 = &test1;
   cout << test1.GetHeight() << endl;
   cout << test1.GetWidth() << endl;
   cout << test1.CalculateSurface() << endl;
 
-  test1.SetWidth(10);
-  test1.SetHeight(4);
-  cout << test1.GetHeight() << endl;
-  cout << test1.GetWidth() << endl;
-  cout << test1.CalculateSurface() << endl;
-
-  Circle test2(53);
+  Circle test2(53), *t2 = &test2;
   cout << test2.GetHeight() << endl;
   cout << test2.GetWidth() << endl;
   cout << test2.CalculateSurface() << endl;
 
-  test2.SetRadius(4);
-  cout << test2.GetHeight() << endl;
-  cout << test2.GetWidth() << endl;
-  cout << test2.CalculateSurface() << endl;
+  vector<Shape*> Shapes;
+
+  Shapes.push_back(t0);
+  Shapes.push_back(t1);
+  Shapes.push_back(t2);
+
+  for (int i = 0; i < Shapes.size(); i++) {
+    cout << Shapes[i]->CalculateSurface() << endl;
+  }
+
 }
